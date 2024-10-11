b2b4f442501349d03c8774a5a5314d06d11c096c
Nong AnuchitO <anuchito.code@gmail.com>
2024-10-11 20:26:48 +0700

    nvim: install harpoon for quick switch between file

--- nvim/lua/anuchito/packer.lua
+++ nvim/lua/anuchito/packer.lua
@@ -22,5 +22,13 @@ return require('packer').startup(function(use)
   })
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/playground', {})
+
+  use "nvim-lua/plenary.nvim"
+  use {
+	  "ThePrimeagen/harpoon",
+	  branch = "harpoon2",
+	  requires = { {"nvim-lua/plenary.nvim"} }
+  }
 
 end)
