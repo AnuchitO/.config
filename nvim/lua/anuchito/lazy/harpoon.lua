9625496802c9a7007651271bd24408cb49d9a61d
Nong AnuchitO <anuchito.code@gmail.com>
2024-11-02 14:53:48 +0700

    nvim: config harpoon file navigator key to navigate around

--- nvim/lua/anuchito/lazy/harpoon.lua
+++ nvim/lua/anuchito/lazy/harpoon.lua
@@ -8,12 +8,13 @@ return {
 
         -- Key mappings
         local map = vim.keymap.set
---        map("n", "<C-e>", function() require("harpoon.ui").toggle_quick_menu() end, { desc = "Toggle Harpoon menu" })
+        map("n", "<C-e>", function() require("harpoon.ui").toggle_quick_menu() end, { desc = "Toggle Harpoon menu" })
+        -- map("n", "<C-e>", "e: Telescope harpoon marks<CR>", { desc = "Open harpoon window" })
         map("n", "<leader>a", function() require("harpoon.mark").add_file() end, { desc = "Add file to Harpoon" })
         map("n", "<leader>r", function() require("harpoon.mark").rm_file() end, { desc = "Remove file from Harpoon" })
         map("n", "<C-h>", function() require("harpoon.ui").nav_file(1) end, { desc = "Go to Harpoon file 1" })
         map("n", "<C-t>", function() require("harpoon.ui").nav_file(2) end, { desc = "Go to Harpoon file 2" })
-        map("n", "<C-n>", function() require("harpoon.ui").nav_next() end, { desc = "Next Harpoon file" })
+        map("n", "<C-i>", function() require("harpoon.ui").nav_next() end, { desc = "Next Harpoon file" })
         map("n", "<C-p>", function() require("harpoon.ui").nav_prev() end, { desc = "Previous Harpoon file" })
 
         -- Optional: Toggle previous & next buffers in the Harpoon list
@@ -36,7 +37,6 @@ return {
             }):find()
         end
 
-        map("n", "<C-e>", "e: Telescope harpoon marks<CR>", { desc = "Open harpoon window" })
 --         map("n", "<C-e>", function() toggle_telescope(harpoon.list()) end, { desc = "Open harpoon window" })
     end,
 }
