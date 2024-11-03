e4177f54e77b702ed9334dd5e994460cc70839a2
Nong AnuchitO <anuchito.code@gmail.com>
2024-11-03 20:17:01 +0700

    nvim: fugitive keymap

--- nvim/lua/anuchito/lazy/fugitive.lua
+++ nvim/lua/anuchito/lazy/fugitive.lua
@@ -4,10 +4,29 @@ return {
         local bufnr = vim.api.nvim_get_current_buf()
         local opts = { buffer = bufnr, remap = false }
 
+        local function current_branch()
+            return vim.fn.system("git rev-parse --abbrev-ref HEAD"):gsub("%s+", "") -- Remove any trailing whitespace
+        end
+
         vim.g.fugitive_no_maps = 1 -- Disable default mappings
         vim.keymap.set("n", "<leader>gst", vim.cmd.Git)
 
         -- always pull rebase with command <leader>ggpur
-        vim.keymap.set("n", "<leader>ggpur", ":Git pull --rebase<CR>", { desc = "Git Pull with Rebase" })
+        vim.keymap.set("n", "<leader>ggpur", function()
+            -- push origin current branch
+            local cmd = string.format("Git push origin %s", current_branch())
+            vim.cmd(cmd)
+        end, { desc = "Git Pull with Rebase" })
+        vim.keymap.set("n", "<leader>gp", function()
+            -- Push to the current branch
+            local cmd = string.format("Git push origin %s", current_branch())
+
+            -- Execute the command
+            vim.cmd(cmd)
+        end, {})
+
+        -- git stash push czz
+        vim.keymap.set("n", "<leader>gsp", ":Git stash push<CR>", { desc = "Git Stash Push" })
+        vim.keymap.set("n", "<leader>gsl", ":Git stash pop<CR>", { desc = "Git Stash List" })
     end
 }
