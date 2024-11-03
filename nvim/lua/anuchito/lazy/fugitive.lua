f2fdb9ee17bcf95202d624bda282106da2048a37
Nong AnuchitO <anuchito.code@gmail.com>
2024-11-03 20:51:11 +0700

    nvim: remap fugitive

--- nvim/lua/anuchito/lazy/fugitive.lua
+++ nvim/lua/anuchito/lazy/fugitive.lua
@@ -17,15 +17,13 @@ return {
             local cmd = string.format("Git pull -r origin %s", current_branch())
             vim.cmd(cmd)
         end, { desc = "Git Pull with Rebase" })
+
         vim.keymap.set("n", "<leader>gp", function()
             -- push origin current branch
             local cmd = string.format("Git push origin %s", current_branch())
-
-            -- Execute the command
             vim.cmd(cmd)
-        end, {})
+        end, { desc = "Git Push" })
 
-        -- git stash push czz
         vim.keymap.set("n", "<leader>czz", ":Git stash push<CR>", { desc = "Git Stash Push" })
         vim.keymap.set("n", "<leader>czp", ":Git stash pop<CR>", { desc = "Git Stash Pop" })
     end
