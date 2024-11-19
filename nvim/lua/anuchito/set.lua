9e4be90ef0ac97da6d5328d2576f99361160446d
Nong AnuchitO <anuchito.code@gmail.com>
2024-11-19 21:48:28 +0700

    nvim: config 2 spaces for tab on js, ts, json

--- nvim/lua/anuchito/set.lua
+++ nvim/lua/anuchito/set.lua
@@ -10,6 +10,15 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+-- Set specific settings for JavaScript and TypeScript files
+vim.cmd [[
+augroup JsTsIndentation
+    autocmd!
+    autocmd FileType javascript,javascriptreact,typescript,typescriptreact,json,yaml,yml setlocal tabstop=2 shiftwidth=2 softtabstop=2 expandtab
+augroup END
+]]
+
+
 vim.opt.smartindent = true
 
 -- line wrap
