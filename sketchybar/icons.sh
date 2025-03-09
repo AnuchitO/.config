3abc01eaabca77148ba095b734a9d913c3798162
AnuchitO <3372362+AnuchitO@users.noreply.github.com>
2025-03-09 22:42:19 +0700

    sketchybar:  version 1 working
    Author:    AnuchitO <>
    Committer: AnuchitO

---
+++ sketchybar/icons.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+# General Icons
+LOADING=􀖇
+APPLE=􀣺
+PREFERENCES=􀺽
+ACTIVITY=􀒓
+LOCK=􀒳
+BELL=􀋚
+BELL_DOT=􀝗
+
+# Git Icons
+GIT_ISSUE=􀍷
+GIT_DISCUSSION=􀒤
+GIT_PULL_REQUEST=􀙡
+GIT_COMMIT=􀡚
+GIT_INDICATOR=􀂓
+
+# Spotify Icons
+SPOTIFY_BACK=􀊎
+SPOTIFY_PLAY_PAUSE=􀊈
+SPOTIFY_NEXT=􀊐
+SPOTIFY_SHUFFLE=􀊝
+SPOTIFY_REPEAT=􀊞
+
+# Yabai Icons
+YABAI_STACK=􀏭
+YABAI_FULLSCREEN_ZOOM=􀏜
+YABAI_PARENT_ZOOM=􀥃
+YABAI_FLOAT=􀢌
+YABAI_GRID=􀧍
+
+# Battery Icons
+BATTERY_100=􀛨
+BATTERY_75=􀺸
+BATTERY_50=􀺶
+BATTERY_25=􀛩
+BATTERY_0=􀛪
+BATTERY_CHARGING=􀢋
+
+# Volume Icons
+VOLUME_100=􀊩
+VOLUME_66=􀊧
+VOLUME_33=􀊥
+VOLUME_10=􀊡
+VOLUME_0=􀊣
+
+# System Applications
+FINDER=􀈕              # Finder (folder icon)
+TERMINAL=􀉿            # Terminal (console window)
+SYSTEM_SETTINGS=􀍟    # System Settings (gear)
+SAFARI=􀎓              # Safari (compass)
+MAIL=􀍜                # Mail (envelope)
+CALENDAR=􀉉            # Calendar (calendar page)
+CONTACTS=􀉧            # Contacts (person card)
+NOTES=􀓕              # Notes (pencil and paper)
+REMINDERS=􀡅          # Reminders (checkmark list)
+MESSAGES=􀌤           # Messages (speech bubble)
+FACETIME=􀒙           # FaceTime (video camera)
+PHOTOS=􀏅             # Photos (flower-like image icon)
+MUSIC=􀑪              # Music (musical note)
+PODCASTS=􀦰           # Podcasts (mic with sound waves)
+TV=􀵪                 # TV (television screen)
+NEWS=􀇕               # News (newspaper)
+
+# Productivity Applications
+PAGES=􀉇              # Pages (document with pen)
+NUMBERS=􀌇            # Numbers (spreadsheet grid)
+KEYNOTE=􀊈            # Keynote (play triangle, symbolizing presentations)
+PREVIEW=􀎽            # Preview (magnifying glass over image)
+TEXTEDIT=􀓟          # TextEdit (pen and paper)
+
+# Developer Tools
+XCODE=􀤏              # Xcode (hammer)
+ACTIVITY_MONITOR=􀒓  # Activity Monitor (pulse)
+DISK_UTILITY=􀤂       # Disk Utility (hard drive)
+TIME_MACHINE=􀣂      # Time Machine (clock with arrow)
+
+# Creative Applications
+GARAGEBAND=􀑫         # GarageBand (guitar)
+IMOVIE=􀽇             # iMovie (film strip)
+FINAL_CUT_PRO=􀽇     # Final Cut Pro (film strip, same as iMovie but contextual)
+LOGIC_PRO=􀑪         # Logic Pro (musical note, contextual)
+
+# Utility Applications
+CALCULATOR=􀓐         # Calculator (plus sign)
+CLOCK=􀐯              # Clock (clock face)
+WEATHER=􀇑            # Weather (cloud with sun)
+MAPS=􀙋               # Maps (map pin)
+
+# Communication Applications
+SLACK=􀙇              # Slack (chat bubble with hashtag)
+ZOOM=􀒞              # Zoom (video camera with magnify)
+TEAMS=􀙤             # Microsoft Teams (two people)
+DISCORD=􀙤           # Discord (two people, contextual)
+
+# Browser Alternatives
+CHROME=􀎐             # Chrome (circle with center dot)
+FIREFOX=􀤽           # Firefox (globe with orbit)
+EDGE=􀤽              # Edge (globe, contextual)
+
+# File Management
+DROPBOX=􀈢           # Dropbox (box with arrow)
+GOOGLE_DRIVE=􀈢      # Google Drive (box, contextual)
+ONE_DRIVE=􀈢         # OneDrive (box, contextual)
+
+# Security
+KEYCHAIN=􀟒          # Keychain Access (key)
+LOCKED=􀒳            # Generic locked state (lock)
+UNLOCKED=􀒴          # Generic unlocked state (unlocked lock)
+
+# Miscellaneous
+TRASH=􀈑              # Trash (trash can)
+SPOTLIGHT=􀊨          # Spotlight (magnifying glass)
+SIRI=􀙫               # Siri (mic with waves)
---
+++ sketchybar/items/spaces.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+
+sketchybar --add event aerospace_workspace_change
+
+spaces=()
+for sid in $(aerospace list-workspaces --all); do
+    # sketchybar --add item space.$sid left \
+    #     --subscribe space.$sid aerospace_workspace_change \
+    #     --set space.$sid \
+    #     background.color=0x44ffffff \
+    #     background.corner_radius=5 \
+    #     background.height=20 \
+    #     background.drawing=off \
+    #     label="$sid" \
+    #     click_script="aerospace workspace $sid" \
+    #     script="$CONFIG_DIR/plugins/aerospace.sh $sid"
+
+    # space=(
+    #     associated_space=$sid
+    #     # icon=$sid
+    #     # icon.padding_left=10
+    #     # icon.padding_right=15
+    #     # padding_left=2
+    #     # padding_right=2
+    #     # label.padding_right=20
+    #     # icon.highlight_color=$RED
+    #     # label.font="sketchybar-app-font:Regular:16.0"
+    #     label.background.drawing=off
+    #     label.background.height=26
+    #     label.background.color=$BACKGROUND_2
+    #     label.background.corner_radius=8
+    #     # label.drawing=off
+    #     #label="$sid"
+    #     #click_script="aerospace workspace $sid"
+    #     #script="$PLUGIN_DIR/aerospace.sh"
+    # )
+
+    space=(
+        icon.highlight_color=$RED
+        icon.padding_left=10
+        icon.padding_right=15
+        icon="$sid"
+        icon.font="$FONT:Bold:16.0"
+        label.padding_right=20
+        padding_left=2
+        padding_right=2
+
+        # label.font="sketchybar-app-font:Regular:16.0"
+        label.font="$FONT:Semibold:13.0"
+        label.background.color=$BACKGROUND_2
+        label.background.corner_radius=8
+        label.background.height=26
+        label.background.drawing=off
+        label.drawing=off
+        label="$YABAI_FLOAT"
+        click_script="aerospace workspace $sid"
+        script="$CONFIG_DIR/plugins/aerospace.sh $sid"
+    )
+
+    sketchybar --add item space.$sid left    \
+               --subscribe space.$sid aerospace_workspace_change \
+               --set space.$sid "${space[@]}"
+
+done
+
+# SPACE_ICONS=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15")
+
+# # Destroy space on right click, focus space on left click.
+# # New space by left clicking separator (>)
+
+# sid=0
+# spaces=()
+# for i in "${!SPACE_ICONS[@]}"
+# do
+#   sid=$(($i+1))
+
+#   space=(
+#     associated_space=$sid
+#     icon=${SPACE_ICONS[i]}
+#     icon.padding_left=10
+#     icon.padding_right=15
+#     padding_left=2
+#     padding_right=2
+#     label.padding_right=20
+#     icon.highlight_color=$RED
+#     label.font="sketchybar-app-font:Regular:16.0"
+#     label.background.height=26
+#     label.background.drawing=on
+#     label.background.color=$BACKGROUND_2
+#     label.background.corner_radius=8
+#     label.drawing=off
+#     script="$PLUGIN_DIR/space.sh"
+#   )
+
+#   sketchybar --add space space.$sid left    \
+#              --set space.$sid "${space[@]}" \
+#              --subscribe space.$sid mouse.clicked
+# done
+
+spaces=(
+  background.color=$BACKGROUND_1
+  background.border_color=$BACKGROUND_2
+  background.border_width=2
+  background.drawing=on
+)
+
+separator=(
+  icon=􀆊
+  icon.font="$FONT:Heavy:16.0"
+  padding_left=15
+  padding_right=15
+  label.drawing=off
+  associated_display=active
+  click_script='sketchybar --trigger space_change'
+  icon.color=$WHITE
+)
+
+sketchybar --add bracket spaces '/space\..*/' \
+           --set spaces "${spaces[@]}"        \
+                                              \
+           --add item separator left          \
+           --set separator "${separator[@]}"
