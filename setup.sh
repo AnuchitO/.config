f0c2912dfe2a9756b901ea520db829a4b800101e
AnuchitO <3372362+AnuchitO@users.noreply.github.com>
2025-03-11 18:10:58 +0700

    setup:  config alacritty theme

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Alacritty config
+./alacritty/config.sh
+
+
+# link
 ln -sfn $HOME/.config/aerospace/.aerospace.toml $HOME/.aerospace.toml
 ln -sfn $HOME/.config/zsh/.zshrc $HOME/.zshrc
 
