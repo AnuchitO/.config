4991e4bdbef1b2fde62dd42819f7030ef8265715
AnuchitO <3372362+AnuchitO@users.noreply.github.com>
2025-03-14 17:20:42 +0700

    setup:  symbolic link the config on home with relative path not using full path name from $HOME

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Alacritty config
-./alacritty/config.sh
+# $HOME/.config/alacritty/config.sh
 
 
 # link
-ln -sfn $HOME/.config/aerospace/.aerospace.toml $HOME/.aerospace.toml
-ln -sfn $HOME/.config/zsh/.zshrc $HOME/.zshrc
+ln -sfn .config/aerospace/.aerospace.toml $HOME/.aerospace.toml
+ln -sfn .config/zsh/.zshrc $HOME/.zshrc
+
+ln -sfn .config/git/.gitconfig $HOME/.gitconfig
 
